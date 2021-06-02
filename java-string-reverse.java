f98c949e3e45be9b12920b99d35bc59605f1fb11
Mohit singh <mohitrajputms123@gmail.com>
2021-06-02 10:07:51 +0530

    Create java-string-reverse.java

---
+++ java-string-reverse.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        boolean yes = true;
+        for(int i=0;i<A.length()/2;i++){
+            if( A.charAt(i) != A.charAt(A.length()-i-1)){
+                System.out.println("No");
+                yes = false;
+                break;
+                
+            }
+        }
+        if(yes){
+            System.out.println("Yes");
+        }
+        
+    }
+}
