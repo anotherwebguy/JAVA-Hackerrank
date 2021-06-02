8ef3a2ae4c3b172ed4d86c948f40afee76ea3c37
Mohit singh <mohitrajputms123@gmail.com>
2021-06-02 10:47:30 +0530

    Update java-strings-tokens.java

--- java-strings-tokens.java
+++ java-strings-tokens.java
@@ -5,8 +5,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        // Write your code here.
+        scan.useDelimiter("\\Z");
+        String s = scan.next().trim();
         if (s.length() > 0) {
         String[] tokens = s.split("[!,?._'@\\s]+");
         System.out.println(tokens.length);
